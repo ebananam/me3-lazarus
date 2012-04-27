91be80b5e90cb232fce410a9613e0d74a4aef321
pedro.s.madeira@gmail.com <pedro.s.madeira@gmail.com@b399ef17-fd9e-fb94-560b-7444ca243ea4>
2012-04-27 19:13:25 +0000

    Reintroduced StripSegments but only as place holder for future work;

--- trunk/LazarusProject/LodViewerControl.cs
+++ trunk/LazarusProject/LodViewerControl.cs
@@ -1,12 +1,3 @@
-#region File Description
-//-----------------------------------------------------------------------------
-// SpinningTriangleControl.cs
-//
-// Microsoft XNA Community Game Platform
-// Copyright (C) Microsoft Corporation. All rights reserved.
-//-----------------------------------------------------------------------------
-#endregion
-
 #region Using Statements
 using System.Diagnostics;
 using System.Windows.Forms;
---
+++ trunk/LazarusProject/StripSegments.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pixelmade.Lazarus
+{
+    public class StripSegments : List<KeyValuePair<int,int>>
+    {
+    }
+}
